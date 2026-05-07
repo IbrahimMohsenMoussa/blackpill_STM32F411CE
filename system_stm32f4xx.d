c7c0694bb2c748d1f9f5d0baded8c4976ed3026f
SeifSeifG <seifseif.gamer20@gmail.com>
2026-05-07 15:03:45 +0300

    Clean project: removed build artifacts and added startup changes

--- system_stm32f4xx.d
+++
@@ -1,5 +0,0 @@
-./system_stm32f4xx.o: RTE\Device\STM32F411CEUx\system_stm32f4xx.c \
-  C:\Users\Seif0\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
-  C:\Users\Seif0\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
-  C:\Users\Seif0\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\Seif0\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
